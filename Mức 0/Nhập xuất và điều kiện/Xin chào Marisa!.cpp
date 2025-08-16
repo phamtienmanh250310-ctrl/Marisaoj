b6de6e577cd4b89863e1501cd4331ac3d07a04ba
TIT_manh <phamtienmanh250310@gmail.com>
2025-08-16 18:52:08 +0700

    Create Xin chào Marisa!.cpp

---
+++ Mức 0/Nhập xuất và điều kiện/Xin chào Marisa!.cpp
@@ -0,0 +1,10 @@
+#include <bits/stdc++.h>
+#define ll long long
+#define FOR(i,l,r) for (int i = l; i <= r; i++)
+using namespace std;
+int main() {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0); cout.tie(0);
+    cout << "Hello Marisa!";
+    return 0;
+}
