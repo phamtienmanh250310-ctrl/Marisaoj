e36d2351cbfe3d9b2d2358086bdd6b26d5d396e3
TIT_manh <phamtienmanh250310@gmail.com>
2025-08-16 18:55:21 +0700

    Create Vòng lặp.cpp

---
+++ Mức 0/Vòng lặp/Vòng lặp.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+#define ll long long
+#define FOR(i,l,r) for (int i = l; i <= r; i++)
+using namespace std;
+int main() {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0); cout.tie(0);
+    ll l, r; cin >> l >> r;
+    FOR(i,l,r) cout << i << " ";
+    return 0;
+}
