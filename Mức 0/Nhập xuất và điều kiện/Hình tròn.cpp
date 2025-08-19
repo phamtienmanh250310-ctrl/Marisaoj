fa8da19a39f9469931137ed731895f9a43af1de7
TIT_manh <phamtienmanh250310@gmail.com>
2025-08-19 15:11:38 +0700

    Create Hình tròn.cpp

---
+++ Mức 0/Nhập xuất và điều kiện/Hình tròn.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+#define ll long long
+#define FOR(i,l,r) for (int i = l; i <= r; i++)
+using namespace std;
+int main() {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0); cout.tie(0);
+    double r; cin >> r;
+    cout << setprecision(3) << fixed << r * 2 * 3.14 << " " << r * r * 3.14;
+    return 0;
+}
