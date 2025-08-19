6071ba37e43e9c25df16be26cb28c6ccc1bf6a1a
TIT_manh <phamtienmanh250310@gmail.com>
2025-08-19 15:12:59 +0700

    Create Đoạn thẳng.cpp

---
+++ Mức 0/Nhập xuất và điều kiện/Đoạn thẳng.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+#define ll long long
+#define FOR(i,l,r) for (int i = l; i <= r; i++)
+using namespace std;
+int main() {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0); cout.tie(0);
+    ll a, b, c, d; cin >> a >> b >> c >> d;
+    if (a > b) swap(a, b);
+    if (c > d) swap(c, d);
+    if (a > c) {
+        swap(a, c);
+        swap(b, d);
+    }
+    if (c <= b) cout << "YES";
+    else cout << "NO";
+    return 0;
+}
