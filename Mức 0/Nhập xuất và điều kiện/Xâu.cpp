cce80441c9ba2a3c12fb52a835b1bf6ce42b6244
TIT_manh <phamtienmanh250310@gmail.com>
2025-08-19 15:09:39 +0700

    Create Xâu.cpp

---
+++ Mức 0/Nhập xuất và điều kiện/Xâu.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+#define ll long long
+#define FOR(i,l,r) for (int i = l; i <= r; i++)
+using namespace std;
+int main() {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0); cout.tie(0);
+    string s; getline(cin, s);
+    FOR(i,1,3) cout << s << endl;
+    return 0;
+}
